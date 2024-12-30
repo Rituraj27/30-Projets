c49c8752c045dde55ccf39162ff10743418dabc8
Rituraj Hao <ri2rajhao1@gmail.com>
2024-12-30 16:44:07 +0530

    Todo List pushed

---
+++ Todo List/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const todoInput = document.getElementById("todo-input");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (todoInput.value === "") {
+    alert("Please enter a task");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = todoInput.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  todoInput.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+const saveData = () => {
+  localStorage.setItem("data", listContainer.innerHTML);
+};
+
+const showtask = () => {
+  listContainer.innerHTML = localStorage.getItem("data");
+};
+
+showtask();
